60ede16f3628baaacfd86385975eb33a1c1309ce
EdithOrt <eoc.diseno@gmail.com>
2021-12-19 13:19:45 -0600

    item detail with count

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ItemCount from "./ItemCount";
 
 export default function ItemDetail({ item }) {
+  const [stock, setStock] = useState(10);
+  const initValue = 1;
+
+  const onAdd = (count, setCount) => {
+    console.log(`Has agregado ${count} piezas`);
+    let result = stock - count;
+    setStock(result);
+    setCount(0);
+  };
+
+  console.log(`Quedan ${stock} piezas en el stock`);
   return (
     <div className="item-detail" id={item.id}>
       <img
@@ -13,6 +26,8 @@ export default function ItemDetail({ item }) {
         <p className="item-detail__text__name">{item.name}</p>
         <p className="item-detail__text__price">{`$ ${item.price}`}</p>
         <p className="item-detail__text__description">{item.description}</p>
+
+        <ItemCount stock={stock} initValue={initValue} onAdd={onAdd} />
       </div>
     </div>
   );
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,22 +1,9 @@
 import { useState, useEffect } from "react";
 
 import productsPromise from "../mock/products";
-import ItemCount from "./ItemCount";
 import ItemList from "./ItemList";
 
 export default function ItemListContainer({ greeting }) {
-  const [stock, setStock] = useState(10);
-  const initValue = 1;
-
-  const onAdd = (count, setCount) => {
-    console.log(`Has agregado ${count} piezas`);
-    let result = stock - count;
-    setStock(result);
-    setCount(0);
-  };
-
-  console.log(`Quedan ${stock} piezas en el stock`);
-
   const [items, setItems] = useState(undefined);
 
   useEffect(() => {
@@ -33,8 +20,6 @@ export default function ItemListContainer({ greeting }) {
     <div>
       <p>{greeting}</p>
       {items ? <ItemList items={items} /> : <p>Loading...</p>}
-
-      <ItemCount stock={stock} initValue={initValue} onAdd={onAdd} />
     </div>
   );
 }
