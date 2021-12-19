b36c8beec4a6a3a59a88e504ed563ddb977bc1cb
EdithOrt <eoc.diseno@gmail.com>
2021-12-19 10:57:36 -0600

    fix code count

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,36 +1,42 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function ItemCount({count, setCount}) {
-    const handleCount = (typeOfOperation) =>{
-        if (typeOfOperation === 'onAdd'){
-            if(count.initialValue < 20){
-                setCount({
-                    ...count,
-                    initialValue: ++count.initialValue 
-                })
-            } else {
-                console.log('ya no hay stock')
-            }
-        } else if (typeOfOperation === 'onRemove'){
-            if(count.initialValue > 1){
-                setCount({
-                    ...count,
-                    initialValue: --count.initialValue
-                })
-            } else {
-                console.log('ya no se puede disminuir')
-            }
-        }
+export default function ItemCount({ stock, initValue, onAdd }) {
+  const [count, setCount] = useState(initValue);
+
+  const handleCount = (typeOfOperation) => {
+    if (typeOfOperation === "onAdd") {
+      count < stock && setCount(count + 1);
+    } else if (typeOfOperation === "onRemove") {
+      count > 1 && setCount(count - 1);
     }
-    return (
-        <div className='item-count'>
-            <button className='item-count__button' onClick={() => handleCount('onRemove')}>-</button>
+  };
+  return (
+    <div className="item-count-container">
+      <div className="item-count">
+        <button
+          className="item-count__button"
+          onClick={() => handleCount("onRemove")}
+          disabled={count <= 1}
+        >
+          -
+        </button>
 
-            <div className='item-count__container-quantity'>
-                <p>{count.initialValue}</p>
-            </div>
-
-            <button className='item-count__button' onClick={() => handleCount('onAdd')}>+</button>
+        <div className="item-count__container-quantity">
+          <p>{count}</p>
         </div>
-    )
+
+        <button
+          className="item-count__button"
+          onClick={() => handleCount("onAdd")}
+          disabled={count >= stock}
+        >
+          +
+        </button>
+      </div>
+
+      <button onClick={() => onAdd(count)} className="button-on-add">
+        Agregar al carrito
+      </button>
+    </div>
+  );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,15 +1,18 @@
-import {useState} from 'react'
-import ItemCount from './ItemCount'
+import React from "react";
+import ItemCount from "./ItemCount";
 
-export default function ItemListContainer({greeting}) {
-    const stock = 20
+export default function ItemListContainer({ greeting }) {
+  const stock = 10;
+  const initValue = 1;
 
-    const [count, setCount] = useState({initialValue: 1, stock: stock})
-    return (
-        <div>
-            <p>{greeting}</p>
+  const onAdd = (count) => {
+    console.log(`Has agregado ${count} piezas`);
+  };
+  return (
+    <div>
+      <p>{greeting}</p>
 
-            <ItemCount count={count} setCount={setCount} />
-        </div>
-    )
+      <ItemCount stock={stock} initValue={initValue} onAdd={onAdd} />
+    </div>
+  );
 }
