d7b1b459f440ba4c7a2d6896c11c70e160b919c4
EdithOrt <eoc.diseno@gmail.com>
2021-12-19 11:38:58 -0600

    infinite loop in products promise fixed

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import productsPromise from "../mock/products";
 import ItemCount from "./ItemCount";
 import ItemList from "./ItemList";
 
@@ -9,66 +11,16 @@ export default function ItemListContainer({ greeting }) {
 
   const [items, setItems] = useState(undefined);
 
-  const productsPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve([
-        {
-          id: 1,
-          title: "Product 1",
-          description:
-            "Proin tempus leo a sapien euismod, ut maximus dolor euismod. Cras elementum enim id quam vulputate, sit amet blandit nunc posuere.",
-          price: 100,
-          pictureUrl:
-            "https://s3.amazonaws.com/kc20-img2-kichink/items_289130_105173_20150612141132_b.jpg",
-        },
-        {
-          id: 2,
-          title: "Product 2",
-          description:
-            "Proin tempus leo a sapien euismod, ut maximus dolor euismod. Cras elementum enim id quam vulputate, sit amet blandit nunc posuere.",
-          price: 100,
-          pictureUrl:
-            "https://s3.amazonaws.com/kc20-img2-kichink/items_43501_20571_20151216110413_b.jpg",
-        },
-        {
-          id: 3,
-          title: "Product 3",
-          description:
-            "Proin tempus leo a sapien euismod, ut maximus dolor euismod. Cras elementum enim id quam vulputate, sit amet blandit nunc posuere.",
-          price: 100,
-          pictureUrl:
-            "https://s3.amazonaws.com/kc20-img2-kichink/items_1762953_418909_0_20180425161136_b.jpg",
-        },
-        {
-          id: 4,
-          title: "Product 4",
-          description:
-            "Proin tempus leo a sapien euismod, ut maximus dolor euismod. Cras elementum enim id quam vulputate, sit amet blandit nunc posuere.",
-          price: 100,
-          pictureUrl:
-            "https://s3.amazonaws.com/kc20-img2-kichink/items_1978421_1250985_0_20181024175726_b.jpg",
-        },
-        {
-          id: 5,
-          title: "Product 5",
-          description:
-            "Proin tempus leo a sapien euismod, ut maximus dolor euismod. Cras elementum enim id quam vulputate, sit amet blandit nunc posuere.",
-          price: 100,
-          pictureUrl:
-            "https://s3.amazonaws.com/kc20-img2-kichink/items_1862522_418909_0_20180719130353_b.jpg",
-        },
-      ]);
-    }, 2000);
-  });
-
-  productsPromise.then(
-    (response) => {
-      setItems(response);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  useEffect(() => {
+    productsPromise.then(
+      (response) => {
+        setItems(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
   return (
     <div>
       <p>{greeting}</p>
