8731bb07c9177e85484d893a5047244ef98eaf95
EdithOrt <eoc.diseno@gmail.com>
2022-01-29 11:24:56 -0600

    clean code

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Item({ id, title, description, price, pictureUrl }) {
   return (
